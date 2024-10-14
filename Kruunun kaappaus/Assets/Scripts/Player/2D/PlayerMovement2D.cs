fa116513ae91d0c94e7c78fce8c1a3bf0b81b6ff
Ivan-Msv <s2200853@edu.bc.fi>
2024-10-14 23:09:22 +0300

    Lisäsin pelaajan ja assetit

---
+++ Kruunun kaappaus/Assets/Scripts/Player/2D/PlayerMovement2D.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D playerCollider;
+    private bool canJump;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float maxCoyoteTime;
+    private float coyoteTimer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        CoyoteCheck();
+        Jump();
+        AxisMovement();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Time.timeScale = 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Time.timeScale = 1;
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.down, 0.1f, ground);
+    }
+    private void AxisMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+    private void CoyoteCheck()
+    {
+        if (!IsGrounded())
+        {
+            //tarkistaa että coyotetime toimii vaan jos yrität hyppää tippumisen jälkeen (ton koko pointti)
+            bool falling = rb.velocity.y <= 0;
+            switch (falling)
+            {
+                case true:
+                    coyoteTimer += Time.deltaTime;
+                    if (coyoteTimer > maxCoyoteTime)
+                    {
+                        canJump = false;
+                    }
+                    break;
+                case false:
+                    canJump = false;
+                    break;
+            }
+        }
+        else
+        {
+            canJump = true;
+            coyoteTimer = 0;
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.velocity = Vector3.up * jumpHeight;
+        }
+    }
+}
---
+++ Kruunun kaappaus/Assets/Scripts/Player/2D/PlayerSprite2D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprite2D : MonoBehaviour
+{
+    void Start()
+    {
+        // change sprite based on selection from playerinfo ? probably
+    }
+}
